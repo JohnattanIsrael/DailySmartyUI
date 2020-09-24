980ee5a246f9e7f57ae13e16d850bc8166f9d294
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-09-24 15:33:04 -0500

    modified serachbar component to take onSum¿mit function via props

--- src/components/results.js
+++ src/components/results.js
@@ -3,12 +3,18 @@ import Logo from '../components/logo';
 import SearchBar from './searchBar';
 
 class Results extends Component {
+
+
+    handleSearchBarSubmit (query) {
+        console.log(query);
+    }
+
     render() {
         return (
             <div>
             
                 <Logo size={55}/>
-                <SearchBar />
+                <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query) } />
 
             </div>
         );
