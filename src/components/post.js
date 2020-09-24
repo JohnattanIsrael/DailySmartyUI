bcb139ccdc7a7c17b706a6f8e397c6f4cc175af9
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-09-24 13:55:38 -0500

    created post component

---
+++ src/components/post.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+    render() {
+        return (
+            <div>
+                <li>
+                    {this.props.title}
+                </li>
+            </div>
+        );
+    }
+}
+
+
+export default Post;
