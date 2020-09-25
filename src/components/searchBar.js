0d6284a4feb6031419da09e1cf98f193ce3a3888
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-09-25 12:33:30 -0500

    added styles to the searchBar component and added a variables scss file to handle colors

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -5,7 +5,7 @@ import { withRouter } from 'react-router-dom';
 
 class SearchBar extends Component {
 
-    handleFormSubmit = function({query}) {
+    handleFormSubmit = function ({ query }) {
         this.props.onSubmit(query);
     }
 
@@ -17,14 +17,17 @@ class SearchBar extends Component {
         const { handleSubmit } = this.props;
         return (
             <form className='search-bar' onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-                <Field name='query' component={this.renderInput}/>
+                <div className='search-bar__wrapper'>
+                    <Field name='query' component={this.renderInput} />
+                    <p>Press return to search</p>
+                </div>
             </form>
         );
     }
 }
 
 SearchBar = reduxForm({
-    form:'searchBar'
+    form: 'searchBar'
 })(SearchBar);
 
 SearchBar = withRouter(SearchBar);
