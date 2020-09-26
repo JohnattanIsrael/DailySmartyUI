353b3cff059516d83ea0814a14bc7df292f4e114
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-09-25 22:18:50 -0500

    finish styles for results

--- src/components/post.js
+++ src/components/post.js
@@ -23,7 +23,7 @@ class Post extends Component {
                 <div className='post-link' key={index}>
                     <div className='post-link__box'></div>
                     <div className='post-link__link'>
-            <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+                        <a href={post_link.link_url}>Useful Link #{index + 1}</a>
                     </div>
                 </div>
             )
@@ -45,30 +45,31 @@ class Post extends Component {
             )
         } else if (this.props.type == 'result') {
             return (
-                <li className='result-post'>
+                <li className='result-post'
+                    onMouseEnter={() => this.setState({ height: 70 })}
+                    onMouseLeave={() => this.setState({ height: 0 })}
+                >
                     <div className='result-post__topics'>
                         {this.renderTopics()}
                     </div>
-                    
+
                     <div className='result-post__title'>
-                        <a 
-                        href={this.props.url_for_post}
-                        onMouseEnter={() => this.setState({ height : 70 })}
-                        onMouseLeave={() => this.setState({ height : 0 })}
+                        <a
+                            href={this.props.url_for_post}
                         >
                             {this.props.title}
                         </a>
                     </div>
 
                     <AnimateHeight
-                    duration={500}
-                    height={this.state.height}
+                        duration={500}
+                        height={this.state.height}
                     >
-                    <div className='result-post__links'>
-                        {this.renderLinks()}
-                    </div>
+                        <div className='result-post__links'>
+                            {this.renderLinks()}
+                        </div>
                     </AnimateHeight>
-                    
+
 
                 </li>
             )
